760a598c3c1aab1a0717b004814b7aa91978b343
HamZa <smha.hmza@gmail.com>
2025-08-02 19:26:57 +0300

    .

--- main.py
+++ main.py
@@ -43,6 +43,4 @@ def buildDecisionSteps(steps: list[dict]):
 
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app)
